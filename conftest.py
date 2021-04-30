1d5d850fb13f1887be5a40b5af9ff0cace66c70d
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2021-04-30 17:32:28 +0200

    chore(conftest): include docstring into conftest.py

--- conftest.py
+++ conftest.py
@@ -1,7 +1,9 @@
+"""This is the all so famous conftest.py which is available in all pytest runs"""
 from nbval.plugin import IPyNbFile
 
 
 def pytest_collectstart(collector):
+    """Exclude those outputs from nbval's comparisons that change from run to run"""
     if isinstance(collector, IPyNbFile):
         collector.skip_compare += 'stderr', \
                                   'application/javascript', \
