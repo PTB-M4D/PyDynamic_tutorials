fc9590ef71eec08ee8561c079783138e454e2460
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2023-02-10 17:00:07 +0100

    refactor(docs): set language to english

--- docs/conf.py
+++ docs/conf.py
@@ -135,7 +135,7 @@ author = "Sascha Eichstädt, Björn Ludwig, Maximilian Gruber"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
